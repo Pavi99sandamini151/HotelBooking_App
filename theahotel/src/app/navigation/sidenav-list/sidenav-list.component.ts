27e958b04397884a6dc5e26f38aeca6224108acd
Pavi99 <pavithra@smbapps.com.au>
2022-12-20 22:43:05 +0530

    changes commit

--- theahotel/src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ theahotel/src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-sidenav-list',
   templateUrl: './sidenav-list.component.html',
   styleUrls: ['./sidenav-list.component.css']
 })
-export class SidenavListComponent {
+export class SidenavListComponent implements OnInit{
+
+  @Output() closeSideNavigation = new EventEmitter();
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onToggleClose () {
+    this.closeSideNavigation.emit();
+  }
 
 }
