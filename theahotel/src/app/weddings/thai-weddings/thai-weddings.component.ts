ff8795f6fa331cf2f8f643e1dfca5a09406c610c
Pavi99 <pavithra@smbapps.com.au>
2022-12-20 20:22:40 +0530

    changes commit

---
+++ theahotel/src/app/weddings/thai-weddings/thai-weddings.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-thai-weddings',
+  templateUrl: './thai-weddings.component.html',
+  styleUrls: ['./thai-weddings.component.css']
+})
+export class ThaiWeddingsComponent {
+
+}
